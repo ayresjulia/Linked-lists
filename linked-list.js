aee44d5e9235cc134d473436a4873a35bffaf4a4
Julia Ayres <j.bobrineva@gmail.com>
2021-04-18 16:20:13 -0400

    added push method to linked list, test passed

---
+++ linked-list.js
@@ -0,0 +1,36 @@
+/** Node: node for a singly linked list. */
+
+class Node {
+	constructor (val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+/** LinkedList: chained together nodes. */
+
+class LinkedList {
+	constructor (vals = []) {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+
+		for (let val of vals) this.push(val);
+	}
+
+	/** push(val): add new value to end of list. */
+
+	push (val) {
+		let newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+	}
+}
+
+module.exports = LinkedList;
